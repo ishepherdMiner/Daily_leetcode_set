4ec3472111051bb11e777a2a9d9fcfad181728ff
Jason <iJason92@yahoo.com>
2017-02-06 23:18:39 +0800

    lengthOfLongestSubstring

--- Daily_leetcode_set/Daily_leetcode_set/AlgorithmsSolution.swift
+++ Daily_leetcode_set/Daily_leetcode_set/AlgorithmsSolution.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class AlgorithmsSolution: NSObject {
     /*
-     * Status: OK
+     * Status: Accepted
      *
      ********************************************************
      * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
@@ -40,7 +40,7 @@ class AlgorithmsSolution: NSObject {
     }
     
     /*
-     * Status: OK
+     * Status: Accepted
      *
      ********************************************************
      * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
@@ -98,6 +98,26 @@ class AlgorithmsSolution: NSObject {
     /*
      * Status: Time Limit Exceeded
      *
+     * Swift 试了好多种,都超时
+     * http://xfhnever.com/2014/10/30/algorithm-lnrs/ 
+     * http://www.geeksforgeeks.org/length-of-the-longest-substring-without-repeating-characters/
+     * http://blog.csdn.net/suool/article/details/38360653
+     * 
+     * 有用Java写的,有python的,有C++的
+     *
+     * 题目讨论中的C++方法写的,能通过
+     * 
+     * vector<int> dict(256, -1);
+     * int maxLen = 0, start = -1;
+     * for (int i = 0; i != s.length(); i++) {
+     *    if (dict[s[i]] > start)
+     *    start = dict[s[i]];
+     *    dict[s[i]] = i;
+     *    maxLen = max(maxLen, i - start);
+     * }
+     * return maxLen;
+     * 移植成Swift还是超时,我只能暂时认为是Swift本身的执行效率原因吧
+     *
      ********************************************************
      * Given a string, find the length of the longest substring without repeating characters.
      * Examples:
@@ -106,50 +126,112 @@ class AlgorithmsSolution: NSObject {
      * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
      */
     func lengthOfLongestSubstring(_ s: String) -> Int {
-        var cur = ""
-        var count = cur.lengthOfBytes(using: String.Encoding.utf8)
-        var rs = [String]()
-        var step = s.startIndex
-        var sub = s
-        while true {
-            var sign = true
-            var notfindTimes = 0
-            sub.characters.forEach({ (c) in
-                if let i = (cur.characters.index(of: c)) {
-                    if sign {
-                        step = s.index(step, offsetBy: cur.distance(from: cur.startIndex, to: i)+1 - notfindTimes, limitedBy: s.endIndex)!
-                        rs.append(cur)
-                        cur.removeAll()
-                        sign = false
-                    }
-                }else {
-                    if sign {
-                        cur.append(c)
-                        notfindTimes += 1
-                        step = s.index(step, offsetBy: 1, limitedBy:s.endIndex)!
-                    }
-                }
-            })
-            
-            sub = s.substring(from: step)
-            if sub.lengthOfBytes(using: String.Encoding.utf8) == 0 {
-                if cur.lengthOfBytes(using: String.Encoding.utf8) > 0 {                    
-                    rs.append(cur)
-                }
-                break
-            }
+        var visited = [Int:Int]()
+        for i in 0..<256 {
+            visited[i] = -1
         }
+        var maxLen = 0
+        var start = -1
         
-        rs.forEach { (s) in
-            let sLength = s.lengthOfBytes(using: String.Encoding.utf8)
-            if sLength > count {
-                count = sLength
+        for i in 0..<s.lengthOfBytes(using: String.Encoding.utf8) {
+            let cur = Int((String(s[s.index(s.startIndex, offsetBy: i)]).unicodeScalars.first?.value)!)
+            if visited[cur]! > start {
+                start = visited[cur]!
             }
+            visited[cur]! = i
+            maxLen = maxLen > i - start ? maxLen : i - start
         }
-        
-        return count
+        return maxLen
     }
 }
+
+/*
+ if s.isEmpty {
+ return 0
+ }
+ 
+ if s.lengthOfBytes(using: String.Encoding.utf8) == 1 {
+ return 1
+ }
+ var maxLen = 0
+ var lastIndex = 0
+ var current = 1
+ var map = [String:Int]()
+ map[s.substring(to:s.index(s.startIndex, offsetBy: 1))] = 0
+ 
+ for i in 1..<s.lengthOfBytes(using: String.Encoding.utf8) {
+ let curString = String(s[s.index(s.startIndex, offsetBy: i)])
+ if map[curString] == nil {
+ map[curString] = i
+ current += 1
+ }else {
+ if lastIndex <= map[curString]! {
+ current = i - map[curString]!
+ lastIndex = map[curString]! + 1
+ map.removeValue(forKey: curString)
+ map[curString] = i
+ }else {
+ current += 1
+ map.removeValue(forKey: curString)
+ map[curString] = i
+ }
+ }
+ 
+ if current > maxLen {
+ maxLen = current
+ }
+ }
+ return maxLen
+ */
+
+/*
+ var cur = ""
+ var count = cur.lengthOfBytes(using: String.Encoding.utf8)
+ var rs = [String]()
+ var step = s.startIndex
+ var sub = s
+ while true {
+ var sign = true
+ var notfindRepeatTimes = 0
+ sub.characters.forEach({ (c) in
+ // appear repeat character
+ if let i = (cur.characters.index(of: c)) {
+ if sign {
+ // - 下一次的匹配位置是关键
+ 
+ step = s.index(step, offsetBy: cur.distance(from: cur.startIndex, to: i)+1 - notfindRepeatTimes, limitedBy: s.endIndex)!
+ rs.append(cur)
+ cur.removeAll()
+ sign = false
+ }
+ }else {
+ if sign {
+ cur.append(c)
+ notfindRepeatTimes += 1
+ step = s.index(step, offsetBy: 1, limitedBy:s.endIndex)!
+ }
+ }
+ })
+ 
+ sub = s.substring(from: step)
+ if sub.lengthOfBytes(using: String.Encoding.utf8) == 0 {
+ if cur.lengthOfBytes(using: String.Encoding.utf8) > 0 {
+ rs.append(cur)
+ }
+ break
+ }
+ }
+ 
+ rs.forEach { (s) in
+ let sLength = s.lengthOfBytes(using: String.Encoding.utf8)
+ if sLength > count {
+ count = sLength
+ }
+ }
+ 
+ return count
+
+ */
 
 public class ListNode {
     public var val: Int
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Daily_leetcode_set
+
+[LeetCode算法题](https://leetcode.com)
+
+
+|功能|方法名|状态|
+|:-:|:-:|:-:|
+|[Two Sum](https://leetcode.com/problems/two-sum/)|twoSum|✅|
+|[Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)|addTwoNumbers|✅|
+|[Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)|lengthOfLongestSubstring|C++可以,移植成Swift超时|
+
